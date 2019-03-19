1f3a2f2b5ab7ecf23e208e2367b80e8164b3f9cf
Amaury <amaucourtois@gmail.com>
2019-03-19 12:54:14 +0100

    pause midi 19/03

---
+++ Les fonctions/Exercice/Exercice.js
@@ -0,0 +1,54 @@
+'strict use'
+
+//Faire une fonction qui retourne bonjour suivi de votre prénom
+function prenom() {
+    return "Bonjour suivi de votre prénom";
+}
+console.log(prenom());
+
+function YO(name) {
+    return "Bonjour " + name;
+}
+console.log(YO("Momo"));
+
+
+//Créer un tableau contenant, des strings et des numbers
+array = ["Roger", 5, "Novak", 1, "Rafa", 2];
+console.log(array);
+
+
+// Créer une fonction qui rangera les nombres dans un tableau de nombres et les strings dans un tableau de strings
+function tablesort(zz) {
+    let array1 = [];
+    let array2 = [];
+    let array3 = [];
+
+    zz.forEach(element => {
+        if (typeof(element) == "string") {
+            array1.push(element);
+        } else {
+            array2.push(element);
+        }
+    });
+    array3.push(array1, array2)
+    return array3
+}
+console.log(tablesort(array));
+
+// correction :
+let tablesort1 = function (zz) {
+    let arraystring = [];
+    let arraynumber = [];
+    let arrayresult = [];
+
+    zz.forEach(element => {
+        if (typeof (element) === string) {
+            arraystring.push(element);
+        } else {
+            arraynumber.push(element);
+        }
+    });
+    arrayresult.push(arraynumber, arraystring);
+
+    return arrayresult;
+}
