2c93ae4ff939e906b98ce4fc1517eef4d6264b43
Amaury <amaucourtois@gmail.com>
2019-04-02 10:57:33 +0200

    test commit via webstorm

---
+++ Node/module_npm/index.js
@@ -0,0 +1,12 @@
+
+const mod = require('./module.js');
+const dawan = require ('@tonno/dawan');
+
+console.log(mod.bonjour());
+console.log(mod.aurevoir());
+
+
+
+dawan.Saluer();
+dawan.Bye();
+console.log(dawan.allUser());
---
+++ Node/module_npm/module.js
@@ -0,0 +1,16 @@
+// new Object()
+// Class es6
+
+// objet litteral
+const objet = {};
+
+objet.bonjour = function (){
+    return "Salut, Dawan c'est bien"
+};
+
+objet.aurevoir = function(){
+    return "bye bye"
+};
+
+// commande pour exporter nos modules
+module.exports = objet;
