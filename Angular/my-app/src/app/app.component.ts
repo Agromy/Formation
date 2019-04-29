71c6b7689da55ad298b50977d84f28f7ab1fa883
Amaury <amaucourtois@gmail.com>
2019-04-29 16:23:55 +0200

    angular j1

--- Angular/my-app/src/app/app.component.ts
+++ Angular/my-app/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {Task, Task2, TaskStatus} from './task'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+/* mon composant appComponent a un attribu/propiété : title dont la valeur est une chaine de caractere
+*/
 export class AppComponent {
-  title = 'my-app';
+  task: Task ={
+    name : 'Learn Angular',
+    status : TaskStatus.Doing,
+    deadLine: new Date ('2019-05-28')
+  };
+
+  task2: Task2 ={
+    name : 'ou pas',
+    status : TaskStatus.Done,
+    deadLine: new Date ('2019-05-28'),
+    doer: 'jlhfyitfv'
+  }
+
 }
+
+
